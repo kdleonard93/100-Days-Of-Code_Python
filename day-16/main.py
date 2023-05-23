3a7156af45e5026b1dfc77204c653af43f2b0d4d
Kyle <kdleonard@icloud.com>
2023-05-23 17:37:13 -0500

    Finished Day 16

--- day-16/main.py
+++ day-16/main.py
@@ -1,26 +1,21 @@
-# from turtle import Screen, Turtle
+from money_machine import MoneyMachine
+from coffee_maker import CoffeeMaker
+from menu import Menu, MenuItem
 
-# timmy = Turtle()
-# print(timmy)
-# timmy.shape("turtle")
-# timmy.color("red")
-# timmy.forward(100)
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+is_on = True
 
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
-
-from prettytable import PrettyTable
-
-table = PrettyTable()
-
-# x.field_names = ["City name", "Area", "Population", "Annual Rainfall"]
-
-table.add_column("Pokemon Name",
-                 ["Pika", "Char", "Squirt"])
-table.add_column("Type",
-                 ["Electric", "Fire", "Water"])
-
-table.align = "l"
-
-print(table)
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if (coffee_maker.is_resource_sufficient(drink)) and (money_machine.make_payment(drink.cost)):
+            coffee_maker.make_coffee(drink)
