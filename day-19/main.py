010fbddde85e56b7ec2090cbb965da9da55687a7
Kyle <kdleonard@icloud.com>
2023-06-01 22:28:59 -0500

    Completed Etch-A-Sketch Challenge

---
+++ day-19/main.py
@@ -0,0 +1,35 @@
+from turtle import Turtle, Screen
+import random
+
+tim = Turtle()
+screen = Screen()
+tim.shape("circle")
+
+def move_forward():
+    tim.forward(10)
+    
+def move_backwards():
+    tim.back(10)
+    
+def counter_clockwise():
+    tim.left(10)
+    
+def clockwise():
+    tim.right(10)
+    
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+screen.listen()
+screen.onkey(key="d", fun=move_forward)
+screen.onkey(key="a", fun=move_backwards)
+screen.onkey(key="w", fun=counter_clockwise)
+screen.onkey(key="s", fun=clockwise)
+screen.onkey(key="c", fun=clear)
+
+
+
+screen.exitonclick()
