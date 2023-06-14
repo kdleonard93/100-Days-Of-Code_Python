30a015cb05b7908658f7098a4e51e16c43a813ff
Kyle <kdleonard@icloud.com>
2023-06-13 21:08:21 -0500

    Setup the paddles

---
+++ day-22/paddle.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+# STARTING_POSITION = (350, 0)
+# MOVE_PADDLE = 20
+
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.color("white")
+        self.penup()
+        self.goto(position)
+        
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+    
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+        
+
+
+
+        
