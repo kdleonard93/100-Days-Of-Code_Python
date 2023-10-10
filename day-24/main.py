5cb4ff215cfeb88cf50c1f9f51907f75bdbd67bd
Kyle <kdleonard@icloud.com>
2023-10-10 07:39:15 -0500

    Mail Merger Project Completed

--- day-24/main.py
+++ day-24/main.py
@@ -1,8 +1,13 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+PLACEHOLDER = "[name]"
+        
+with open("/Users/kyleleonard/100-Days-Of-Code_Python/day-24/Input/Names/invited_names.txt") as names_file:
+   names = names_file.readlines()
+   print(names)
+   
+with open("/Users/kyleleonard/100-Days-Of-Code_Python/day-24/Input/Letters/starting_letter.txt") as letter_file:
+    letter_content = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_content.replace(PLACEHOLDER, stripped_name)
+        with open(f"/Users/kyleleonard/100-Days-Of-Code_Python/day-24/Output/ReadyToSend/letter_for_{stripped_name}", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
