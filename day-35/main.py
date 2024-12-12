3f059c723a8adfac2cb617bdd11e9ede0ba2efca
Kyle <kdleonard@icloud.com>
2024-12-11 22:38:47 -0600

    Majority of logic is complete
    Just need to convert this to sending an SMS instead of printing in console.

--- day-35/main.py
+++ day-35/main.py
@@ -7,11 +7,24 @@ API_KEY = "7b23a84e98f9cd37635be245ee371a18"
 params = {
     "lat": 41.881832,
     "lon": -87.623177,
+    "cnt": 4,
     "appid": API_KEY,
 }
 
 response = requests.get(OWM_ENDPOINT, params=params)
 response.raise_for_status()
 data = response.json()
-five_day_forecast = data["list"]
-print(five_day_forecast)
+five_day_forecast = data
+
+will_rain = False
+
+for forecast in five_day_forecast['list']:
+    weather_id = forecast["weather"][0]["id"]
+    
+    if int(weather_id) < 700:
+        will_rain = True
+        
+if will_rain:
+    print("Bring an unbrella")
+else:
+    print("All Clear")
