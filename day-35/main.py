7fa3b874700acdb7ec8f7e37f20eb767357c9b0e
Kyle <kdleonard@icloud.com>
2024-12-11 23:44:11 -0600

    Got the SMS to work, regenerated and moved API keys into .env file

--- day-35/main.py
+++ day-35/main.py
@@ -1,14 +1,23 @@
 import requests
+from os import environ
+from dotenv import load_dotenv
+from twilio.rest import Client 
+
+load_dotenv()  
+
+OWM_API = environ.get("OWM_API_KEY")
+TWILIO_API = environ.get("TWILIO_API_KEY")
+TWILIO_SID = environ.get("TWILIO_SID_KEY")
 
 
 OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
-API_KEY = "7b23a84e98f9cd37635be245ee371a18"
+
 
 params = {
     "lat": 41.881832,
     "lon": -87.623177,
     "cnt": 4,
-    "appid": API_KEY,
+    "appid": OWM_API,
 }
 
 response = requests.get(OWM_ENDPOINT, params=params)
@@ -25,6 +34,18 @@ for forecast in five_day_forecast['list']:
         will_rain = True
         
 if will_rain:
-    print("Bring an unbrella")
+    client = Client(TWILIO_SID, TWILIO_API)
+    message = client.messages.create(
+        body="☔️IT'S GON RAIN!☔️",
+        from_="+18885998708",
+        to="+16302012552"
+    )
+    print(message.status)
 else:
-    print("All Clear")
+    client = Client(TWILIO_SID, TWILIO_API)
+    message = client.messages.create(
+        body="☀️Sunny ma boi!☀️",
+        from_="+18885998708",
+        to="+16302012552"
+    )
+    print(message.status)
