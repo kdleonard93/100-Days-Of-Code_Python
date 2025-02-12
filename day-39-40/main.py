0c8db345497fb3f69dcc526eb14312a303c019a5
Kyle <kdleonard@icloud.com>
2025-02-11 22:27:45 -0600

    FlightSearch class complete
    module instructions led to some errors. The API is sus and needs multiple attempts to actually hit the flight endpoints successfully on the free account.

--- day-39-40/main.py
+++ day-39-40/main.py
@@ -1,14 +1,18 @@
 from pprint import pprint
 from data_manager import DataManager
+from flight_search import FlightSearch
+import time
+
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
 
-if sheet_data[0]["iataCode"] == "":
-    from flight_search import FlightSearch
-    flight_search = FlightSearch()
-    for row in sheet_data:
+for row in sheet_data:
+    if row["iataCode"] == "":
         row["iataCode"] = flight_search.get_destination_code(row["city"])
-    pprint(f"sheet_data:\n {sheet_data}")
+        # slowing down requests to avoid rate limit
+        time.sleep(2)
+print(f"sheet_data:\n {sheet_data}")
 
-    data_manager.destination_data = sheet_data
-    data_manager.update_destination_codes()
+data_manager.destination_data = sheet_data
+data_manager.update_destination_codes()
