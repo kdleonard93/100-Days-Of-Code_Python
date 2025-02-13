b185ea192cf3d22b97a321ccac8257fa12617b9e
Kyle <kdleonard@icloud.com>
2025-02-12 19:56:58 -0600

    Project 99.99% complete
    Will prob move on. Still having issues with Twilio not actually sending the sms but i don't get any error on my end.

--- day-39-40/flight_search.py
+++ day-39-40/flight_search.py
@@ -62,7 +62,7 @@ class FlightSearch:
             except requests.exceptions.HTTPError as e:
                 print(f"Attempt {attempt + 1} failed for {city_name}: {e}")
                 if attempt < retries - 1:
-                    time.sleep(4)  # Wait before retrying
+                    time.sleep(4)
                     continue
                 print(
                     f"Max retries reached for {city_name}. Returning 'Not Found'.")
@@ -70,3 +70,33 @@ class FlightSearch:
             except (KeyError, IndexError) as e:
                 print(f"Error parsing response for {city_name}: {e}")
                 return "Not Found"
+            
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        headers = {"Authorization": f"Bearer {self._token}"}
+        query = {
+            "originLocationCode": origin_city_code,
+            "destinationLocationCode": destination_city_code,
+            "departureDate": from_time.strftime("%Y-%m-%d"),
+            "returnDate": to_time.strftime("%Y-%m-%d"),
+            "adults": 1,
+            "nonStop": "true",
+            "currencyCode": "GBP",
+            "max": "10",
+        }
+
+        response = requests.get(
+            url=flight_endpoint,
+            headers=headers,
+            params=query,
+        )
+
+        if response.status_code != 200:
+            print(f"check_flights() response code: {response.status_code}")
+            print("There was a problem with the flight search.\n"
+                  "For details on status codes, check the API documentation:\n"
+                  "https://developers.amadeus.com/self-service/category/flights/api-doc/flight-offers-search/api"
+                  "-reference")
+            print("Response body:", response.text)
+            return None
+
+        return response.json()
