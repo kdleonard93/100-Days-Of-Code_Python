59bd9ad6c132116761a93f70ff48bcf693bdef6b
Kyle L <kdleonard@icloud.com>
2025-06-04 22:12:37 -0500

    complete
    Using the recommended tool instead

--- day-48/main.py
+++ day-48/main.py
@@ -1,26 +1,62 @@
-# import selenium 
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-from firecrawl import FirecrawlApp
-from os import environ
-from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
-load_dotenv()
 
-FC_API_KEY = environ.get("FC_API_KEY")
+# Optional - Keep the browser open (helps diagnose issues if the script crashes)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
 
-app = FirecrawlApp(api_key=FC_API_KEY)
+driver = webdriver.Chrome(options=chrome_options)
 
-scrape_results = app.scrape_url("https://digitaldopamine.dev", formats=['markdown', 'html'])
+driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
-print(scrape_results)
+# Get cookie to click on.
+cookie = driver.find_element(by=By.ID, value="cookie")
 
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_experimental_option("detach", True)
-# driver = webdriver.Chrome(options=chrome_options)
+# Get upgrade item ids.
+items = driver.find_elements(by=By.CSS_SELECTOR, value="#store div")
+item_ids = [item.get_attribute("id") for item in items]
 
-# driver.get("https://digitaldopamine.dev")
-# driver_header_3 = driver.find_element(By.TAG_NAME, "h3")
-# print(driver_header_3.text)
+timeout = time.time() + 5
+five_min = time.time() + 60*5  # 5 minutes
 
-# driver.quit()
+while True:
+    cookie.click()
+
+    if time.time() > timeout:
+        all_prices = driver.find_elements(by=By.CSS_SELECTOR, value="#store b")
+        item_prices = []
+
+        for price in all_prices:
+            element_text = price.text
+            if element_text != "":
+                cost = int(element_text.split("-")[1].strip().replace(",", ""))
+                item_prices.append(cost)
+
+        cookie_upgrades = {}
+        for n in range(len(item_prices)):
+            cookie_upgrades[item_prices[n]] = item_ids[n]
+
+        money_element = driver.find_element(by=By.ID, value="money").text
+        if "," in money_element:
+            money_element = money_element.replace(",", "")
+        cookie_count = int(money_element)
+
+        affordable_upgrades = {}
+        for cost, id in cookie_upgrades.items():
+            if cookie_count > cost:
+                affordable_upgrades[cost] = id
+
+        highest_price_affordable_upgrade = max(affordable_upgrades)
+        print(highest_price_affordable_upgrade)
+        to_purchase_id = affordable_upgrades[highest_price_affordable_upgrade]
+
+        driver.find_element(by=By.ID, value=to_purchase_id).click()
+
+        timeout = time.time() + 5
+
+    if time.time() > five_min:
+        cookie_per_s = driver.find_element(by=By.ID, value="cps").text
+        print(cookie_per_s)
+        break
